e7efa35cd7b04bd5d094b480191a3469166e8fbd
SDP Team 3 Lighthouse 6 <anthonymende@umass.edu>
2019-11-19 15:24:56 -0500

    Starting function pointers for BME280 read, write, and delay

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -416,6 +416,73 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+/*
+ *	@brief Function Pointer for Delaying the BME280.
+ *	After a number of milliseconds have passed, we
+ *	return control.
+ * 	@param[in] milliseconds : How much to delay by in milliseconds.
+ */
+void bme280_user_delay_ms(uint32_t milliseconds)
+{
+	//TODO: Fill in
+}
+
+/*
+ *	@brief Function Pointer for reading data from the BME280 using the I2C protocol.
+ * 	@param[in] dev_id : Can be used as a variable to store the I2C address of the device.
+ * 	@param[in] reg_addr : Register address of what we want to read in from the BME280.
+ * 	@param[out] reg_data : Data we're reading out from the register.
+ * 	@param[in] len : Length of the data we're reading out.
+ */
+int8_t bme280_user_i2c_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len)
+{
+	/*
+	 * Data on the bus should be like
+	 * |------------+---------------------|
+	 * | I2C action | Data                |
+	 * |------------+---------------------|
+	 * | Start      | -                   |
+	 * | Write      | (reg_addr)          |
+	 * | Stop       | -                   |
+	 * | Start      | -                   |
+	 * | Read       | (reg_data[0])       |
+	 * | Read       | (....)              |
+	 * | Read       | (reg_data[len - 1]) |
+	 * | Stop       | -                   |
+	 * |------------+---------------------|
+	 */
+	//TODO: Fill in
+	int8_t rslt = 0; /* Return 0 for Success, non-zero for failure */
+	return rslt;
+}
+
+/*
+ *	@brief Function Pointer for writing data to the BME280 using the I2C protocol.
+ * 	@param[in] dev_id : Can be used as a variable to store the I2C address of the device.
+ * 	@param[in] reg_addr : Register address of what we want to read in from the BME280.
+ * 	@param[out] reg_data : Data we're reading out from the register.
+ * 	@param[in] len : Length of the data we're reading out.
+ */
+int8_t bme280_user_i2c_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len)
+{
+	/*
+	 * Data on the bus should be like
+	 * |------------+---------------------|
+	 * | I2C action | Data                |
+	 * |------------+---------------------|
+	 * | Start      | -                   |
+	 * | Write      | (reg_addr)          |
+	 * | Write      | (reg_data[0])       |
+	 * | Write      | (....)              |
+	 * | Write      | (reg_data[len - 1]) |
+	 * | Stop       | -                   |
+	 * |------------+---------------------|
+	 */
+
+	//TODO: Fill in
+	int8_t rslt = 0; /* Return 0 for Success, non-zero for failure */
+	return rslt;
+}
 /* USER CODE END 4 */
 
 /**
